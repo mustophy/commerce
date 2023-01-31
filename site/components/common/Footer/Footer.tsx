e1c826cbd811e9d956c1f0e3dde8a8cc2748dcf0
mustophy <54689763+mustophy@users.noreply.github.com>
2023-01-31 15:00:09 +0100

    Update Footer.tsx

--- site/components/common/Footer/Footer.tsx
+++ site/components/common/Footer/Footer.tsx
@@ -36,7 +36,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
                 <span className="rounded-full border border-accent-6 mr-2">
                   <Logo />
                 </span>
-                <span>ACME</span>
+                <span>OHOVISUALS</span>
               </a>
             </Link>
           </div>
